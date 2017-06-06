81affbdb2f73dfe8398a6b15532387cb1db51728
Kasper Munch <kaspermunch@birc.au.dk>
2017-06-06 09:17:21 +0200

    Added installation structure and files.

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(name='GenomicWindows',
+      version='1.0',
+      description='Utilities for working with intervals in separate chromosomes.',
+      url='https://github.com/kaspermunch/GenomicIntervals',
+      author='Kasper Munch',
+      packages=['GenomicIntervals'],
+      zip_safe=True)
+

15abc6b24e5b4f77dc12ca8e298abd1185140d6b
Kasper Munch <kaspermunch@birc.au.dk>
2017-06-11 21:45:53 +0200

    Fixed wrong name in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 from setuptools import setup
 
-setup(name='GenomicWindows',
+setup(name='GenomicIntervals',
       version='1.0',
       description='Utilities for working with intervals in separate chromosomes.',
       url='https://github.com/kaspermunch/GenomicIntervals',
